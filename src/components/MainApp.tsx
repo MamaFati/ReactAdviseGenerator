c001d7a2019ea1d8946cf459c2733e51519e32fc
MamaFati <abufati876@gmail.com>
2024-11-29 19:31:42 +0000

    refactor: modify ui add preview section

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -1,33 +1,50 @@
-import { useEffect, useCallback } from "react";
-import Message from "./Message";
+import { useEffect, useCallback, useState } from "react";
+// import Message from "./Message";
 import axios from "axios";
 import useAdviceStore from "../store/stateStore";
-import AdviceHistory from "./AdviceHistor";
 
 export default function MainApp() {
   const { advice, setAdvice } = useAdviceStore();
-  // const storeAdvice = advice;
+  const [advices, setAdvices] = useState<string[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const getAdvice = useCallback(async () => {
     const result = await axios.get(import.meta.env.VITE_API_URL);
-
-    setAdvice(result.data.slip.advice);
+    const newAdvice = result.data.slip.advice;
+    setAdvices((prevAdvices) => [...prevAdvices, newAdvice]);
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+    setAdvice(newAdvice);
   }, [setAdvice]);
-  useEffect(
-    function () {
-      getAdvice();
-    },
-    [getAdvice]
-  );
-  return (
-    <div className="text-center ">
-      <h1 className="px-10">{advice}</h1>
-      <button onClick={getAdvice} className="my-5">
-        Get Advice
-      </button>
 
-      <Message />
-      <AdviceHistory />
+  useEffect(() => {
+    getAdvice();
+  }, [getAdvice]);
+
+  const displayPrevAdvice = () => {
+    setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : 0));
+    setAdvice(advices[currentIndex - 1]);
+  };
+
+  const displayNextAdvice = () => {
+    if (currentIndex < advices.length - 1) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+      setAdvice(advices[currentIndex + 1]);
+    } else {
+      getAdvice();
+    }
+  };
+
+  return (
+    <div className="text-center">
+      <h1 className="px-10">{advices[currentIndex]}</h1>
+      <button onClick={displayPrevAdvice} className="my-5">
+        <h1>{"<"}</h1>
+      </button>
+      <button onClick={displayNextAdvice} className="my-5">
+        <h1>{">"}</h1>
+      </button>
+      {/* <Message /> */}
+      {/* // <AdviceHistory /> */}
     </div>
   );
 }
